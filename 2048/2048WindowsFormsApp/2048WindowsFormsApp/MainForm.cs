a9b729d33bc136bcc27c0f44a63b41aa996f009c
FIS <FIS@DESKTOP-5JQ9N5L>
2023-01-15 18:24:15 +0300

    +

---
+++ 2048/2048WindowsFormsApp/2048WindowsFormsApp/MainForm.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+using System.Drawing;
+
+using System.Windows.Forms;
+
+namespace _2048WindowsFormsApp
+{
+    public partial class MainForm : Form
+    {
+        private Label[,] labelsMap;
+        private const int mapSize = 4;
+        private static Random random = new Random();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            InitMap();
+            GenerateNumber();
+        }
+
+
+
+        private void InitMap()
+        {
+            labelsMap = new Label[mapSize, mapSize];
+
+            for (int i = 0; i < mapSize; i++)
+            {
+                for (int j = 0; j < mapSize; j++)
+                {
+                    var newLabel = CreateLabel(i, j);
+                    Controls.Add(newLabel);
+                    labelsMap[i, j] = newLabel;
+                }
+            }
+        }
+
+        private void GenerateNumber()
+        {
+            while (true) //как улучшить
+            {
+                var randomNumberLabel = random.Next(mapSize * mapSize);
+                var indexRow = randomNumberLabel / mapSize;
+                var indexColumn = randomNumberLabel % mapSize;
+                if (labelsMap[indexRow, indexColumn].Text == String.Empty)
+                {
+                    //сделать рандомно 2 или 4
+                    labelsMap[indexRow, indexColumn].Text = "2";
+                }
+                break;
+            }            
+        }
+
+        private Label CreateLabel(int indexRow, int indexColumn)
+        {
+            Label label = new Label();
+            label.BackColor = Color.Silver;
+            label.Font = new Font("Microsoft Sans Serif", 15.75F, FontStyle.Bold, GraphicsUnit.Point, ((byte)(204)));
+            label.Size = new Size(70, 70);
+            label.Text = String.Empty;
+            label.TextAlign = ContentAlignment.MiddleCenter;
+            int x = 10 + indexColumn * 76;
+            int y = 70 + indexRow * 76;
+            label.Location = new Point(x, y);
+            return label;
+        }
+
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Right) 
+            {
+                MessageBox.Show("Правая");
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                MessageBox.Show("Левая");
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                MessageBox.Show("Вверх");
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                MessageBox.Show("Вниз");
+            }
+        }
+    }
+}
