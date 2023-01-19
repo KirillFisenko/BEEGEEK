735bc387787da95a0725150fb278861ff73c4eee
KirillFisenko <79265463469@ya.ru>
2023-01-19 14:35:02 +0300

    никаких больше + выполнил подзадачу 1

--- Test/Test/TestConsoleApp/Program.cs
+++ Test/Test/TestConsoleApp/Program.cs
@@ -5,37 +5,10 @@ using System.Threading;
 
 public class Program
 {
-
     static void Main(string[] args)
     {
-        var n = Console.ReadLine();
-        var balls = Console.ReadLine().Split().ToList();
-        var result = new List<string>();
-        var cnt = 0;
-        for (int i = 1; i < balls.Count; i++)
-        {
-            if (balls[i] == balls[i - 1])
-            {
-                cnt++;
-            }
-        }
-
-
-    }
-
-    while (в balls есть более 2-х подряд)
-    {
-    for (int i = 1; i < balls.Count; i++)
-        {
-        if(в balls есть более 2-х подряд)
-            {
-        result.Add(одинаковые);
-        balls.Remove(одинаковые);
-        }
-    }
-    Console.WriteLine(result.Count());
-
-
+        
+    }    
 }
 
 
