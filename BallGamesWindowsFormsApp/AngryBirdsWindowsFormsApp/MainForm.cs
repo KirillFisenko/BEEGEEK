cedf58913d11fdd509110e1c4ac20cd54fc25ae6
KirillFisenko <79265463469@ya.ru>
2023-03-06 18:09:07 +0300

    создание pig

---
+++ BallGamesWindowsFormsApp/AngryBirdsWindowsFormsApp/MainForm.cs
@@ -0,0 +1,27 @@
+﻿using BallClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AngryBirdsWindowsFormsApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            Pig pig = new Pig(this);
+            pig.Start();            
+        }
+    }
+}
---
+++ BallGamesWindowsFormsApp/BallClassLibrary/Pig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BallClassLibrary
+{   
+    public class Pig : Ball
+    {
+        public Random random = new Random();
+        public Pig(Form form) : base(form)
+        {
+            radius = 50;
+            centerX = random.Next(Center(), RightSide());
+            centerY = random.Next(TopSide(), Center());
+            brush = Brushes.Pink;
+
+            this.form = form;
+            timer = new Timer();
+            timer.Interval = 20;
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Show();
+        }
+    }
+}
