884a53c20434f525331dc5c8ffe2a76654bf24eb
KirillFisenko <79265463469@ya.ru>
2023-03-15 20:01:28 +0300

    +

--- FrogWindowsFormsApp/FrogWindowsFormsApp/MainForm.cs
+++ FrogWindowsFormsApp/FrogWindowsFormsApp/MainForm.cs
@@ -20,7 +20,7 @@ namespace FrogWindowsFormsApp
         private void Swap(PictureBox clickedPicture)
         {
             var distance = Math.Abs(clickedPicture.Location.X - emptyPictureBox.Location.X) / emptyPictureBox.Size.Width;
-            if(distance > 2 || clickedPicture == emptyPictureBox)
+            if (distance > 2 || clickedPicture == emptyPictureBox)
             {
                 MessageBox.Show("Так нельзя");
             }
@@ -33,16 +33,17 @@ namespace FrogWindowsFormsApp
                 userCountSteps++;
                 if (Win())
                 {
-                    if(userCountSteps - minCountSteps == 0)
+                    if (userCountSteps - minCountSteps == 0)
                     {
                         MessageBox.Show($"Вы выиграли за минмиальное количество ходов!");
                     }
                     else
                     {
-                        MessageBox.Show($"Вы выиграли! Ваше количетво ходов: {userCountSteps}. Минимально возможное - {minCountSteps}");
-                    }                    
+                        MessageBox.Show($"Вы выиграли! Ваше количетво ходов: {userCountSteps}. " +
+                            $"Минимально возможное - {minCountSteps}");
+                    }
                 }
-            }            
+            }
         }
 
         private bool Win()
@@ -53,15 +54,6 @@ namespace FrogWindowsFormsApp
                 leftPictureBox4.Location.X > emptyPictureBox.Location.X &&
                 emptyPictureBox.Location.X == 440)
             {
-                leftPictureBox1.Enabled = false;
-                leftPictureBox2.Enabled = false;
-                leftPictureBox3.Enabled = false;
-                leftPictureBox4.Enabled = false;
-                rightPictureBox1.Enabled = false;
-                rightPictureBox2.Enabled = false;
-                rightPictureBox3.Enabled = false;
-                rightPictureBox4.Enabled = false;
-                emptyPictureBox.Enabled = false;
                 return true;
             }
             return false;
@@ -74,7 +66,9 @@ namespace FrogWindowsFormsApp
 
         private void rulesButton_Click(object sender, EventArgs e)
         {
-            MessageBox.Show($"Цель игры — расположить лягушек, которые смотрят влево, в левую часть, а остальных - в правую часть за минимальное количество перепрыгиваний.\r\n\r\nПрыгать можно на листок, если он находится рядом или через 1 лягушку");
+            MessageBox.Show($"Цель игры — расположить лягушек, которые смотрят влево, в левую часть, " +
+                $"а остальных - в правую часть за минимальное количество перепрыгиваний." +
+                $"\r\n\r\nПрыгать можно на листок, если он находится рядом или через 1 лягушку");
         }
     }
 }
